5068fd302adcc0ade64b0bf0c1064d4a946014fd
ralphbarros <ralphbarros@gmail.com>
2021-06-08 20:29:56 -0300

    update commit

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -11,6 +11,10 @@ class UsersRepository implements IUsersRepository{
     constructor(){
         this.repository = getRepository(User);
     }
+    async findByEmail(email: string): Promise<User> {
+        const user = await this.repository.findOne({ email } );
+        return user;
+    }
 
     async create({name,email,driver_license,password}: ICreateUserDTO): Promise<void> {
         const user = this.repository.create({
--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from "tsyringe";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
-
+import {hash} from 'bcrypt';
 
 
 @injectable()
@@ -13,11 +13,15 @@ class CreateUserUseCase {
         ){}
 
         async execute({name, email,driver_license,password }:ICreateUserDTO):Promise<void>{
+            
+            const userAlreadyExists = await this.userRepository.findByEmail(email);
+            
+            const passwordHash = await hash(password,8);
             await this.userRepository.create({
                 name,
                 email,
                 driver_license,
-                password,
+                password: passwordHash,
             });
             console.log("Criado");
         }
