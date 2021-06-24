4d0c7bfb4566189096681874a6ac0574136b1510
ralphbarros <ralphbarros@gmail.com>
2021-06-24 13:40:23 -0300

    create table rentals

--- src/shared/infra/typeorm/migrations/1622898144587-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1622898144587-CreateUsers.ts
@@ -9,7 +9,8 @@ export class CreateUsers1622898144587 implements MigrationInterface {
                 columns: [
                     {
                         name: "id",
-                        type: "uuid",                        
+                        type: "uuid",    
+                        isPrimary:true,                                          
                     },
                     {
                         name: "name",
