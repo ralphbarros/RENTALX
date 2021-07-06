200d8fb67b7cba7f8b4e7189d48f708fcc525c4b
ralphbarros <ralphbarros@gmail.com>
2021-07-06 20:15:18 -0300

    update rental

---
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
@@ -0,0 +1,17 @@
+
+
+
+
+interface IRequest {
+    id: string ;
+    user_id: string;
+
+}
+
+class DevolutionRentalUseCase {
+    async execute ({id, user_id }:IRequest){
+
+    }
+}
+
+export { DevolutionRentalUseCase }
